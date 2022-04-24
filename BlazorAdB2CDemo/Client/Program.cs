6ccaca180d60314b90e27d1c6d6bc0138807b566
Alberto Mori <albi.mo87@hotmail.it>
2022-04-24 22:45:28 +0200

    wip - adding adb2c configuration to client project

--- BlazorAdB2CDemo/Client/Program.cs
+++ BlazorAdB2CDemo/Client/Program.cs
@@ -1,11 +1,22 @@
 using BlazorAdB2CDemo.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddHttpClient("MyServerApiClient", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MyServerApiClient"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("{SCOPE_URI}");
+});
 
 await builder.Build().RunAsync();
